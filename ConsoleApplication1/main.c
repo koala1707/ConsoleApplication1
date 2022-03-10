8a3f6e1ef00b7105be3c6d1ef88809c3f2b028e1
koala1707 <m3tpfbzr@gmail.com>
2022-03-10 10:40:07 +1030

    Pra2 Activity2 Done

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -1,50 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 /* function to read number from standard input (keyboard) */ 
-int getNumber();
-
+void getNumber(int *);
 /* function to calculate the sum of two numbers */
-int calculateSum(int, int);
-
+void calculateSum(int, int, int*);
 /* function to display details to the screen */
 void displayDetails(void);
-
 int main() {
-	int num1 = 0; /* first number to be entered by user */
-	int num2 = 0; /* second number to be entered by user */
-	int sum = 0; /* stores sum of two numbers */
-
+	int x = 10;
+	int y = 3;
+	int z = 0;
+	int* num1 = &x; /* first number to be entered by user */
+	int* num2 = &y; /* second number to be entered by user */
+	int* sum = &z; /* stores sum of two numbers */
 	displayDetails();
 	/* prompt for and read first number */
 	printf("Please enter first number : ");
-	num1 = getNumber();
+	getNumber(num1);
 	/* prompt for and read second number */
 	printf("Please enter second number: ");
-	num2 = getNumber();
+	getNumber(num2);
 	/* add two numbers together and display to screen */
-	sum = calculateSum(num1, num2);
-	printf("\nSum of %d and %d is: %d\n\n", num1, num2, sum);
+	calculateSum(*num1, *num2, sum);
+	printf("\nSum of %d and %d is: %d\n\n", *num1, *num2, *sum);
 	return 0;
 }
 
 /* function to read number from standard input (keyboard) */
-int getNumber() {
-	int num;
-	scanf("%d", &num);
-	return num;
+void getNumber(int *num) {
+	scanf("%d", num);
 }
-
 /* function to calculate the sum of two numbers */
-int calculateSum(int number1, int number2) {
-	return number1 + number2;
+void calculateSum(int number1, int number2, int *sum) {
+	*sum = number1 + number2;
 }
-
-/* function to display details to the screen */
-void displayDetails(void) {
-	printf("Author : Your Name\n");
-	printf("Stud ID : Your Student Id\n");
-	printf("Email ID : Your Email Id\n");
+/* function to display details to the screen */ 
+void displayDetails(void) 
+{ 
+	printf("Author : Erika\n");   
+	printf("Stud ID : 110308922\n");   
+	printf("Email ID : hosey001@mymail.unisa.edu.au\n");   
 	printf("This is my own work as defined by the\n"); 
-	printf("University's Academic Misconduct Policy.\n\n");
-}/* end function displayDetails */
+	printf("University's Academic Misconduct Policy.\n\n"); 
+}
+/* end function displayDetails */
